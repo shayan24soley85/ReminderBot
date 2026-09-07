50855d414cc4b0cadb12a2a57e4a5cc04dcbbf1d
shayan24soley <shayan.soleimani85@gmail.com>
2026-09-07 18:59:34 +0330

    fix : edit name to fix a simple bug

--- keyboards.py
+++ keyboards.py
@@ -2,34 +2,34 @@ import telebot
 
 main_inline_markup = telebot.types.InlineKeyboardMarkup(row_width=2)
 reg_btn = telebot.types.InlineKeyboardButton(
-    "✍️ Registration", callback_data="register_info"
+    "✍️ Information Registration", callback_data="register_info"
 )
 prof_btn = telebot.types.InlineKeyboardButton(
     "👤 My Profile", callback_data="my_profile"
 )
+about_btn = telebot.types.InlineKeyboardButton("ℹ️ About Us", callback_data="about_us")
+help_btn = telebot.types.InlineKeyboardButton("❓ Help", callback_data="help_menu")
 btn_two = telebot.types.InlineKeyboardButton("two", callback_data="btn_two")
 btn_three = telebot.types.InlineKeyboardButton("three", callback_data="btn_three")
-help_btn = telebot.types.InlineKeyboardButton("❓ Help", callback_data="help_menu")
-about_btn = telebot.types.InlineKeyboardButton("ℹ️ About Us", callback_data="about_us")
 
 main_inline_markup.add(reg_btn, prof_btn)
+main_inline_markup.add(about_btn, help_btn)
 main_inline_markup.add(btn_two, btn_three)
-main_inline_markup.add(help_btn, about_btn)
 
-home_btn = telebot.types.InlineKeyboardButton("🏠 Home", callback_data="go_home")
+home_btn = telebot.types.InlineKeyboardButton("🏠 Home", callback_data="home")
+
+home_markup = telebot.types.InlineKeyboardMarkup()
+home_markup.add(home_btn)
 
 about_markup = telebot.types.InlineKeyboardMarkup(row_width=2)
-github_btn = telebot.types.InlineKeyboardButton(
+github_button = telebot.types.InlineKeyboardButton(
     "Github", url="https://github.com/shayan24soley85"
 )
-telegram_btn = telebot.types.InlineKeyboardButton(
+telegram_button = telebot.types.InlineKeyboardButton(
     "Telegram", url="https://t.me/shayan357v"
 )
-about_markup.add(github_btn, telegram_btn)
+about_markup.add(github_button, telegram_button)
 about_markup.add(home_btn)
-
-help_markup = telebot.types.InlineKeyboardMarkup(row_width=1)
-help_markup.add(home_btn)
 
 profile_markup = telebot.types.InlineKeyboardMarkup(row_width=2)
 edit_btn = telebot.types.InlineKeyboardButton("✏️ Edit Info", callback_data="edit_info")
